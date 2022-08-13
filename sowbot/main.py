444c96f077f0334202daa9b9ee6b77c807559af1
tsutsuken <592846+tsutsuken@users.noreply.github.com>
2022-08-13 15:47:59 +0900

    feat: Twitterのタイムラインを監視し、すべてのツイートの本文をprintする

--- sowbot/main.py
+++ sowbot/main.py
@@ -1,1 +1,20 @@
-print('Hello, world!')
+import os
+import tweepy
+from dotenv import load_dotenv
+
+# .envファイルから環境変数を読み込む
+load_dotenv()
+
+class SowBotStream(tweepy.StreamingClient):
+	def on_tweet(self, tweet):
+		print(tweet.text)
+		print("on_tweet")
+
+# Twitter認証情報を環境変数から読み込む
+bearer_token = os.getenv('BEARER_TOKEN')
+
+# タイムラインの監視スタート
+print('Start watching tweets')
+sowBotStream = SowBotStream(bearer_token)
+sowBotStream.sample()
+
